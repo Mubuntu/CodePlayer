b0b2de1a73f8230b5456b3c7fe4c5ee9e0577bab
Mubuntu <patrick.a.frison@gmail.com>
2017-02-19 19:20:04 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+$(".toggleButton").hover(function() {
+    $(this).css("background-color", "grey");
+}, function(){
+  $(this).css("background-color", "#EEEEEE");
+});
+
+function mouseEnter() {
+    $(this).css("background-color", "grey");
+}
+
+function mouseLeave() {
+    $(this).css("background-color", "#EEEEEE");
+
+}
